c6ea639748e8dbad4a5f67e6cbd30797f5a5895a
Rohit Gupta <gupta.rohitg.rohit900@gmail.com>
2025-04-28 17:20:47 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -70,5 +71,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_ROOT = "/opt/render/project/src/staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = BASE_DIR / "staticfiles"  # Where collectstatic will put all files
+
+# WhiteNoise config (optional but recommended)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
